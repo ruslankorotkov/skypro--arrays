360d54f53cdafb69f27b5a4c7b1c270259f4a26a
ruslankorotkov <ruslankorotkov.rus@yandex.ru>
2022-12-16 21:25:09 +0300

    МАССВЫ 2.

--- src/Main.java
+++ src/Main.java
@@ -5,141 +5,70 @@ public class Main {
         task3();
         task4();
         task5();
-        task6();
-        task7();
-        task8();
-        task9();
-        task10();
+    }
+
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
     }
 
     public static void task1() {
-        System.out.println("Задача 1,1");
-        int[] weight = new int[12];
-        weight[0] = 1;
-        weight[1] = 2;
-        weight[2] = 3;
+        System.out.println("Задача 1 ( способ 1)");
+        int[] arr = generateRandomArray();
+        int sum = 0;
+        for (int index = 0; index < arr.length; index++) {
+            sum = sum + arr[index];
+        }
+        System.out.println(" Сумма трат за месяц составила " + sum + " рублей");
     }
 
     public static void task2() {
-        System.out.println("Задача 1,2");
-        double[] weight = {1.57, 7.654, 9.986, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        System.out.println("Задача 1( способ 2");
+        int[] arr = generateRandomArray();
+        int sum = 0;
+        for (int element : arr) {
+            sum = sum + element;
+        }
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей");
     }
 
     public static void task3() {
-        System.out.println("Задача 1,3");
-        double[] box = new double[7];
-        box[0] = 93;
-        box[1] = 74;
-        box[2] = 46.56;
-        box[3] = 3;
-        box[4] = 93;
-        box[5] = 2;
-        box[6] = 13.73;
+        System.out.println("Задача 2");
+        int[] arr = generateRandomArray();
+        int arrMax = 99999;
+        int arrMin = 200000;
+        for (int i = 0; i < arr.length; i++)
+            if (arr[i] > arrMax) {
+                arrMax = arr[i];
+            } else if (arr[i] < arrMin) {
+                arrMin = arr[i];
+            }
+        System.out.println(" Минимальная сумма трат за день составила " + arrMin + " рублей. Максимальная сумма трат за день составила " + arrMax + " рублей ");
     }
 
     public static void task4() {
-        System.out.println("Задача 2,1");
-        int[] weight = new int[12];
-        weight[0] = 1;
-        weight[1] = 2;
-        weight[2] = 3;
-        for (int i = 0; i < weight.length; i++) {
-            System.out.print(weight[i]);
-            if (i != weight.length - 1) {
-                System.out.print(" , ");
-            }
+        System.out.println("Задача 3");
+        double[] arr = {23, 34, 45, 2, 9, 56, 9, 6, 4, 3, 3, 3, 5, 6, 7, 8, 9, 0, 23, 67, 9, 45, 3, 5, 6, 7, 8, 3, 8, 3,};
+        double sum = 0;
+        double arrAverage = 0;
+        for (int index = 0; index < arr.length; index++) {
+            sum = sum + arr[index];
         }
-        System.out.println();
+        arrAverage = sum / arr.length;
+
+
+        System.out.println(" Средняя сумма трат за месяц составила " + arrAverage + " рублей");
     }
 
     public static void task5() {
-        System.out.println("Задача 2,2");
-        double[] weight = {1.57, 7.654, 9.986, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-        for (int i = 0; i < weight.length; i++) {
-            System.out.print(weight[i]);
-            if (i != weight.length - 1) {
-                System.out.print(" , ");
-            }
-        }
-        System.out.println();
-    }
-
-    public static void task6() {
-        System.out.println("Задача 2,3");
-        double[] box = new double[7];
-        box[0] = 93;
-        box[1] = 74;
-        box[2] = 46.56;
-        box[3] = 3;
-        box[4] = 93;
-        box[5] = 2;
-        box[6] = 13.73;
-        for (int i = 0; i < box.length; i++) {
-            System.out.print(box[i]);
-            if (i != box.length - 1) {
-                System.out.print(" , ");
-            }
-        }
-        System.out.println();
-    }
-
-    public static void task7() {
-        System.out.println("Задача 3,1");
-        int[] weight = new int[12];
-        weight[0] = 1;
-        weight[1] = 2;
-        weight[2] = 3;
-        for (int i = weight.length - 1; i >= 0; i--) {
-            System.out.print(weight[i]);
-            if (i != 0) {
-                System.out.print(" , ");
-            }
-        }
-        System.out.println();
-    }
-
-    public static void task8() {
-        System.out.println("Задача 3,2");
-        double[] weight = {1.57, 7.654, 9.986, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-        for (int i = weight.length - 1; i >= 0; i--) {
-            System.out.print(weight[i]);
-            if (i != 0) {
-                System.out.print(" , ");
-            }
-        }
-        System.out.println();
-    }
-
-    public static void task9() {
-        System.out.println("Задача 3,3");
-        double[] box = new double[7];
-        box[0] = 93;
-        box[1] = 74;
-        box[2] = 46.56;
-        box[3] = 3;
-        box[4] = 93;
-        box[5] = 2;
-        box[6] = 13.73;
-        for (int i = box.length - 1; i > 0; i--) {
-            System.out.print(box[i]);
-            if (i != 0) {
-                System.out.print(" , ");
-            }
-        }
-        System.out.println();
-    }
-
-    public static void task10() {
         System.out.println("Задача 4");
-        int[] weight = {1, 2, 3, 4, 5};
-        for (int i = 0; i <= weight.length; i++) {
-            if (i % 2 != 0) {
-                i = i + 1;
-                System.out.print(" " + i + " ");
-                if (i != weight.length + 1) {
-                    System.out.print(" , ");
-                }
-            }
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int index = reverseFullName.length - 1; index > -1; index--) {
+            System.out.print(reverseFullName[index]);
         }
     }
 }
